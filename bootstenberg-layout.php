5e68c61d6b3d4727e58107b0efa6296c984b42b6
slothgeek <58670267+slothgeek@users.noreply.github.com>
2021-06-01 11:14:28 -0600

    Update bootstenberg-layout.php

--- bootstenberg-layout.php
+++ bootstenberg-layout.php
@@ -24,3 +24,22 @@ function create_block_bootstenberg_layout_block_init() {
 	register_block_type_from_metadata( __DIR__ );
 }
 add_action( 'init', 'create_block_bootstenberg_layout_block_init' );
+
+if (is_admin()) {
+	function add_boostrap_js_to_gutenberg(){
+		wp_enqueue_script(
+			'bootstrap-js-cdn',
+			'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js',
+			array(),
+			'1.0',
+			false
+		);
+	}
+	add_action('admin_enqueue_scripts', 'add_boostrap_js_to_gutenberg');
+
+	function add_bootstrap_css_to_gutenberg(){
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css' );
+	}
+	add_action('after_setup_theme', 'add_bootstrap_css_to_gutenberg');
+}
