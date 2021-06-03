6b635c9f403bba2b11ac291d44696aa40d1772dc
SlothGeek <jorge@slothgeek.com>
2021-06-03 16:40:35 -0600

    Mejoras

--- bootstenberg-layout.php
+++ bootstenberg-layout.php
@@ -5,7 +5,7 @@
  * Requires at least: 5.7
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            SlothGeek
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       bootstenberg-layout
